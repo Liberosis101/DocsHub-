82bb46074d3b88161f529fcf01eb82b5f53c5d59
Piyush bera <piyushbera35@gmail.com>
2024-04-09 21:16:05 +0530

    fix background overflow on small devices

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Background from "./components/Background";
 import Foreground from "./components/Foreground";
 const App = () => {
   return (
-    <div className="relative z-10 w-full h-screen bg-zinc-800 overflow">
+    <div className="relative z-10 w-full h-screen bg-zinc-800 overflow-auto">
       <Background />
       <Foreground />
     </div>
